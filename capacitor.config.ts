dbad112ccec9c3e9ef33de7b101b20c045702c23
gowtham-12855 <gowtham-12855@gowtham-12855.csez.zohocorpin.com>
2022-07-20 10:52:54 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SSS',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
